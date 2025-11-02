6525f2c57e13ddc3ec6b613f006300838389d5e9
Ahmed Mohamed <ahmedmohamed00318@gmail.com>
2025-11-03 01:12:41 +0200

    Delete oop/oop directory

--- oop/oop/projects/true_false_quiz/question_model.py
+++
@@ -1,9 +0,0 @@
-class Question:
-    """
-    This class take two input arguments:
-    * q_text --> Question with the answer
-    * q_answer --> if it is tru or false
-    """
-    def __init__(self, q_text = "", q_answer = ""):
-        self.text = q_text
-        self.answer = q_answer
